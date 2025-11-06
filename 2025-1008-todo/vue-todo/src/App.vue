4d5f097c450779891dd69aafbf9f35b02339bb7e
ch-yang1273 <ych1273@gmail.com>
2025-11-06 21:38:55 +0900

    todo .

--- 2025-1008-todo/vue-todo/src/App.vue
+++ 2025-1008-todo/vue-todo/src/App.vue
@@ -1,11 +1,18 @@
-<script setup></script>
+<script>
+import TodoView from '@/views/TodoView.vue'
+
+ export default {
+   name: 'App',
+   components: {
+     TodoView
+   }
+ }
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div>
+    <TodoView />
+  </div>
 </template>
 
 <style scoped></style>
---
+++ 2025-1008-todo/vue-todo/src/views/TodoView.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: 'TodoView',
+  data() {
+    return {
+      todos: [
+        {id: 1, text: '아침먹기', completed: true},
+        {id: 2, text: 'Vue 공부하기', completed: false},
+      ]
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="todo-view">
+    <h1>Todo List</h1>
+    <ul>
+      <li v-for="todo in todos" v-bind:key="todo.id">
+        {{ todo.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
