e40000e85d97dd7d2a06fea5558a00018abff088
ch-yang1273 <ych1273@gmail.com>
2025-11-12 21:34:09 +0900

    Calculator 부분 기능

--- 2025-1106_Calculator/vue-calculator/src/components/calculator/CalculatorDisplay.vue
+++ 2025-1106_Calculator/vue-calculator/src/components/calculator/CalculatorDisplay.vue
@@ -1,16 +1,23 @@
 <script setup>
 const props = defineProps({
-  value: {
+  displayValue: {
+    type: String,
+    default: '0',
+  },
+  displayResult: {
     type: String,
     default: '0',
   },
 });
 </script>
 <template>
-  <div
-    class="mb-5 flex-1 overflow-hidden rounded-lg bg-[#3d3d3d] p-5 text-right text-4xl font-light wrap-break-word text-white"
-  >
-    {{ value }}
+  <div class="mb-5 flex flex-col bg-[#3d3d3d] px-5 py-2">
+    <div
+      class="flex-1 overflow-hidden rounded-lg text-right text-4xl font-light wrap-break-word text-white"
+    >
+      {{ displayValue }}
+    </div>
+    <div class="text-right text-2xl text-gray-400">{{ displayResult }}</div>
   </div>
 </template>
 
--- 2025-1106_Calculator/vue-calculator/src/components/calculator/TheCalculator.vue
+++ 2025-1106_Calculator/vue-calculator/src/components/calculator/TheCalculator.vue
@@ -1,12 +1,81 @@
 <script setup>
 import CalculatorKeypad from './CalculatorKeypad.vue';
 import CalculatorDisplay from './CalculatorDisplay.vue';
+import { computed, ref } from 'vue';
+
+const displayNumber = ref('0');
+const displayOperator = ref('');
+const previousOperator = ref('');
+const displayResult = ref('0');
+
+const handleButtonClick = (label, type) => {
+  console.log('TheCalculator', label, type);
+  if (type === 'clear') {
+    displayNumber.value = '0';
+    displayOperator.value = '';
+    previousOperator.value = '';
+    displayResult.value = '0';
+  } else if (type === 'number') {
+    // operator가 있는 상태에서 숫자가 눌리면 계산
+    if (displayOperator.value) {
+      displayResult.value = calculate(
+        displayResult.value,
+        displayNumber.value,
+        previousOperator.value
+      );
+
+      displayNumber.value = '0';
+      previousOperator.value = displayOperator.value;
+      displayOperator.value = '';
+    }
+
+    displayNumber.value =
+      displayNumber.value === '0' ? label : displayNumber.value + label;
+  } else if (type === 'operator') {
+    displayOperator.value = label;
+  } else if (type === 'equals') {
+    displayResult.value = calculate(
+      displayResult.value,
+      displayNumber.value,
+      previousOperator.value
+    );
+
+    displayNumber.value = '0';
+    previousOperator.value = '';
+    displayOperator.value = '';
+  }
+};
+
+const computedValue = computed(
+  () => displayNumber.value + ' ' + displayOperator.value
+);
+const computedResult = computed(
+  () => displayResult.value + ' ' + previousOperator.value
+);
+
+const calculate = (firstNumber, secondNumber, operator) => {
+  switch (operator) {
+    case '+':
+      return String(Number(firstNumber) + Number(secondNumber));
+    case '-':
+      return String(Number(firstNumber) - Number(secondNumber));
+    case 'x':
+      return String(Number(firstNumber) * Number(secondNumber));
+    case '/':
+      return String(Number(firstNumber) / Number(secondNumber));
+    case '':
+      return displayNumber.value;
+  }
+};
 </script>
 
 <template>
   <div class="flex flex-col">
-    <CalculatorDisplay />
-    <CalculatorKeypad />
+    <CalculatorDisplay
+      v-bind:display-value="computedValue"
+      v-bind:display-result="computedResult"
+    />
+    <CalculatorKeypad v-on:click="handleButtonClick" />
   </div>
 </template>
 
