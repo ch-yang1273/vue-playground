96f1a913c4ee424029dd8ae75632992a6454c9d6
ch-yang1273 <ych1273@gmail.com>
2025-11-19 23:01:59 +0900

    github 계정정보 가져오기

--- 2025-1106_Calculator/vue-calculator/src/components/github/GithubAvatar.vue
+++ 2025-1106_Calculator/vue-calculator/src/components/github/GithubAvatar.vue
@@ -1,11 +1,14 @@
-<script setup></script>
+<script setup>
+defineProps({
+  avatarUrl: {
+    type: String,
+    default: 'https://avatars.githubusercontent.com/u/9919?v=4',
+  },
+});
+</script>
 
 <template>
-  <img
-    src="https://avatars.githubusercontent.com/u/9919?v=4"
-    alt="Profile"
-    class="size-28 rounded-full"
-  />
+  <img v-bind:src="avatarUrl" alt="Profile" class="size-28 rounded-full" />
 </template>
 
 <style scoped></style>
--- 2025-1106_Calculator/vue-calculator/src/components/github/GithubUserInfo.vue
+++ 2025-1106_Calculator/vue-calculator/src/components/github/GithubUserInfo.vue
@@ -1,7 +1,19 @@
-<script setup></script>
+<script setup>
+defineProps({
+  login: {
+    type: String,
+    default: 'octocat',
+  },
+  name: {
+    type: String,
+    default: 'monalisa octocat',
+  },
+});
+</script>
 
 <template>
-  <h2 class="text-2xl text-white">octocat</h2>
+  <h2 class="text-2xl text-white">{{ login }}</h2>
+  <p class="text-white">{{ name }}</p>
 </template>
 
 <style scoped></style>
