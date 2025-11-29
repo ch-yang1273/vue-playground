02814254f9f0d2db6675f523079094876109d7dd
ch-yang1273 <ych1273@gmail.com>
2025-11-29 19:52:58 +0900

    NavBar 생성

---
+++ 2025-1129_vue_route/vue-router/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {RouterLink} from "vue-router";
+</script>
+
+<template>
+  <nav>
+    <RouterLink :to="{name: 'home'}">Home</RouterLink>
+    <RouterLink :to="{name: 'about'}">About</RouterLink>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  display: flex;
+  gap: 30px;
+  justify-content: center;
+}
+</style>
